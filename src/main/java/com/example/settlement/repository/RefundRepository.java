f5e0b26fe3e2012f5efe3e1888d7867bfa52edd5
Junghoon Kim <jhkim2275@gmail.com>
2026-05-08 14:30:58 +0900

    feat: Created a RefundRepository to handle refunds more efficiently

--- src/main/java/com/example/settlement/repository/RefundRepository.java
+++ src/main/java/com/example/settlement/repository/RefundRepository.java
@@ -3,5 +3,12 @@ package com.example.settlement.repository;
 import com.example.settlement.entity.Refund;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+
 public interface RefundRepository extends JpaRepository<Refund, String> {
+    List<Refund> findByRefundedAtBetween(
+            OffsetDateTime start,
+            OffsetDateTime end
+    );
 }
